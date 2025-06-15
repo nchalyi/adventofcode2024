d834ac5ac1706184071dcbd83e397f7352441a77
nchalyi <ch.online.edu@gmail.com>
2025-06-16 00:09:37 +0700

    Day 3 FULL

--- day3/step2.cc
+++ day3/step2.cc
@@ -2,8 +2,24 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <vector>
 #include <regex>
 using namespace std;
+
+// function for splitting line by pattern
+vector<string> splitIgnoreDelimiters(const string text, const regex pattern) {
+    vector<string> parts;
+    sregex_token_iterator it(text.begin(), text.end(), pattern, -1);
+    sregex_token_iterator end;
+    
+    while (it != end) {
+        if (!it->str().empty())
+            parts.push_back(*it);
+        ++it;
+    }
+    
+    return parts;
+}
 
 int main (int argc, char** argv) {
     const string filename = argv[1];
@@ -14,28 +30,23 @@ int main (int argc, char** argv) {
         cerr << "ERR opening file: " << filename << endl;
         return -1;
     }
-
+    line = string((istreambuf_iterator<char>(file)),istreambuf_iterator<char>());
     regex pattern("mul\\((\\d+),(\\d+)\\)");
     int result = 0;
-    while (getline(file, line)) {
-        istringstream iss(line);
+    regex noway("don\\'t\\(\\)[\\s\\S]*?do\\(\\)");
+    auto newline = splitIgnoreDelimiters(line, noway);         
+    for (const auto block : newline) {
+        cout << "step 1 " << block << endl;
+        regex dontcheck("don\\'t\\(\\)[\\s\\S]*$");
+        string inblock = regex_replace(block, dontcheck, "");
+        cout << "step 2 " << inblock << endl;
+        auto inbegin = sregex_iterator(inblock.begin(), inblock.end(), pattern);
+        auto inend = sregex_iterator();
 
-        string text;
-        while ( iss >> text ) {
-            
-            regex noway("don\\'t\\(\\).*?do\\(\\)");
-            
-            string newtext = regex_replace(text, noway, "");
-
-            auto begin = sregex_iterator(newtext.begin(), newtext.end(), pattern);
-            auto end = sregex_iterator();
-
-            for (auto it = begin; it != end; ++it) {
-                cout << (*it)[1].str() << " " << (*it)[2].str() << endl;
-                result += stoi((*it)[1].str())*stoi((*it)[2].str());
-            }
+        for (auto it = inbegin; it != inend; ++it) {
+            cout << (*it)[1].str() << " " << (*it)[2].str() << endl;
+            result += stoi((*it)[1].str())*stoi((*it)[2].str());
         }
-   
     }
     cout << result << endl;
 
